282236eaad567a6f5686b7b4296a309f61977bb4
渡辺　健吾 <kengo978@yahoo.co.jp>
2017-08-20 23:40:37 +0900

    GoForwardUnityChanのはつプッシュ
    音が出るのが少し早い気がする。

---
+++ Assets/blockSound.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blockSound : MonoBehaviour {
+    //アニメーションするためのコンポーネントを入れる
+    Animator animator;
+
+    //地面の位置
+    private float groundLevel = -3.0f;
+
+	// Use this for initialization
+	void Start () {
+
+       
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //着地しているかどうか調べる
+        bool isGround = (transform.position.y < this.groundLevel) ? false : true;
+        this.animator.SetBool("isGround", isGround);
+
+        //設置しているときに、ボリュームを出す
+        GetComponent<AudioSource>().volume = (isGround) ? 0 : 1;
+
+    }
+}
