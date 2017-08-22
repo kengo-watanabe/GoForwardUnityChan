1a1001d74c1ae59748fcf4ea11994c351648ee25
渡辺　健吾 <kengo978@yahoo.co.jp>
2017-08-23 01:05:58 +0900

    GoForwardUnityChan 2プッシュ
    OnCollisionEnterを使い、直してみました。

--- Assets/blockSound.cs
+++ Assets/blockSound.cs
@@ -3,14 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class blockSound : MonoBehaviour {
-    //アニメーションするためのコンポーネントを入れる
-    Animator animator;
+    public AudioClip block;
 
-    //地面の位置
-    private float groundLevel = -3.0f;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 
        
 		
@@ -18,12 +15,16 @@ public class blockSound : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        //着地しているかどうか調べる
-        bool isGround = (transform.position.y < this.groundLevel) ? false : true;
-        this.animator.SetBool("isGround", isGround);
+       
 
-        //設置しているときに、ボリュームを出す
-        GetComponent<AudioSource>().volume = (isGround) ? 0 : 1;
+    }
 
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "cubeTag" || other.gameObject.tag == "groundTag") {
+            AudioSource audioSource = gameObject.GetComponent<AudioSource>();
+            audioSource.clip = block;
+            audioSource.Play();
+        }
     }
 }
